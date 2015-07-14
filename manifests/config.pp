9f37f6f38090374ae5626cf61646b2b70c53c4ba
Clint Savage <herlo1@gmail.com>
2015-07-14 15:48:26 -0600

    add configurations and related templates
    Signed-off-by: Clint Savage <herlo1@gmail.com>

---
+++ manifests/config.pp
@@ -0,0 +1,22 @@
+
+
+class totpcgi::config inherits totpcgi {
+
+  file { $totpcgi_config:
+    ensure  => file,
+    owner   => $totpcgi_owner,
+    group   => $totpcgi_group,
+    mode    => '0640',
+    content => template('totpcgi/totpcgi.conf.erb'),
+  }
+
+  file { $provisioning_config:
+    ensure  => file,
+    owner   => $provisioning_owner,
+    group   => $provisioning_group,
+    mode    => '0640',
+    content => template('totpcgi/provisioning.conf.erb'),
+  }
+
+}
+
