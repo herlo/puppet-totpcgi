a31cd268a9112a7ac56f45b5fba5dcbaacff3631
Clint Savage <herlo1@gmail.com>
2015-08-03 12:47:54 -0600

    initial client configuration
    Signed-off-by: Clint Savage <herlo1@gmail.com>

---
+++ manifests/client.pp
@@ -0,0 +1,32 @@
+# == Class: totpcgi::client
+#
+# Client configuration (pam_url, pincodes, and ssl configs).
+#
+# === Authors
+#
+# Clint Savage <herlo@linuxfoundation.org>
+#
+# === Copyright
+#
+# Copyright 2015 Clint Savage
+#
+# === License
+#
+# @License Apache-2.0 <http://spdx.org/licenses/Apache-2.0>
+#
+
+class totpcgi::client (
+  $host                = $totpcgi::params::host
+) inherits totpcgi::params {
+
+  include totpcgi::repo
+
+  package [ 'pam_url':
+    require => Class['::totpcgi::repo']
+  }
+
+  host { "$totpcgi_host":
+    ip => $totpcgi_host_ip,
+  }
+
+}
