9a918c57512475d1ca947df84d8da956bec47685
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-08-13 09:23:57 -0700

    Enable code coverage reporting
    Turn on the code coverage reporting so it's obvious what still needs
    test work.
    
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,5 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 RSpec.configure do |c|
   c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
 end
+
+at_exit { RSpec::Puppet::Coverage.report! }
